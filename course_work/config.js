b83aaede402382401f986d1acd4172eee556150e
Ilinskyi Ivan <ilyinsky_ivan@yahoo.com>
2021-03-11 16:35:04 +0200

    return old connection string

--- course_work/config.js
+++ course_work/config.js
@@ -1,7 +1,7 @@
 module.exports = 
 {
     ServerPort: process.env["PORT"] || 3002,
-    DataBaseUrl: 'mongodb+srv://ivan_ilinskyi:Ilyinsky02@cluster0.rsd4l.mongodb.net/myFirstDatabase?retryWrites=true&w=majority' || 'mongodb+srv://ivan_ilinskyi:Ilyinsky02@cluster0.rsd4l.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+    DataBaseUrl: process.env["DATABASE_URL"] || 'mongodb+srv://ivan_ilinskyi:Ilyinsky02@cluster0.rsd4l.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
     SessionSecret: 'secret_here',
     ServerSalt: 'S$%KLAiMM+_'
 };
